fb4a066d98c8f0c8191605de3e67a77b7975faba
Ира <zhilyaeva@macbook-pro.local>
2018-10-01 19:46:19 +0300

    Initial Commit

---
+++ Project Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Light
+//
+//  Created by Ира  on 01.10.2018.
+//  Copyright © 2018 Ira Zhilyaeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

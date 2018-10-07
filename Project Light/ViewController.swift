69b9beeff37f3f2959811a8884bd95e7d20adcd2
Ира <zhilyaeva@macbook-pro.local>
2018-10-07 19:45:13 +0300

    Версия фонарика 1.1, добавила иконку и картинку фонарика

--- Project Light/ViewController.swift
+++ Project Light/ViewController.swift
@@ -1,10 +1,14 @@
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+   @IBOutlet weak var textIfNoTorch: UITextView!
+    @IBOutlet weak var FlashLight: UIImageView!
+    @IBOutlet weak var background: UIImageView!
     @IBAction func buttonPressed() {
         isOn = !isOn
     }
- 
+    
     var isOn = true {
         didSet {
             updateUI()
@@ -16,8 +20,26 @@ class ViewController: UIViewController {
         updateUI()
     }
     
+    
     func updateUI() {
-        view.backgroundColor = isOn ? .white : .black
+        let device = AVCaptureDevice.default(for: .video)
+        
+        if let device = device, device.hasTorch{
+            textIfNoTorch.isHidden = true
+            
+            do {
+                try device.lockForConfiguration()
+                device.torchMode = isOn ? .off : .on
+                FlashLight.isHighlighted = !isOn
+                device.unlockForConfiguration()
+            } catch {
+                print (#function, error)
+            }
+        } else {
+            view.backgroundColor = isOn ? .white : .black
+            FlashLight.isHidden = true
+            background.isHidden = true
+        }
     }
     
     override var prefersStatusBarHidden: Bool {
